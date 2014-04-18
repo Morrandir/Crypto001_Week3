47887572de7bcc1c6b7db5bdda77ff2d33980a35
Qubo Song <sqb_007@hotmail.com>
2014-04-18 18:34:13 +0800

    Initial commit.

---
+++ BlockHash/BlockHash/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace BlockHash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_Browse = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.progressBar);
+            this.groupBox1.Controls.Add(this.textBox_Result);
+            this.groupBox1.Controls.Add(this.button_Browse);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(565, 172);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose a file to reveal its SHA256 hash";
+            // 
+            // button_Browse
+            // 
+            this.button_Browse.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button_Browse.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button_Browse.Location = new System.Drawing.Point(478, 43);
+            this.button_Browse.Name = "button_Browse";
+            this.button_Browse.Size = new System.Drawing.Size(81, 23);
+            this.button_Browse.TabIndex = 0;
+            this.button_Browse.Text = "Browse";
+            this.button_Browse.UseVisualStyleBackColor = true;
+            this.button_Browse.Click += new System.EventHandler(this.button_Browse_Click);
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Result.Location = new System.Drawing.Point(6, 45);
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.ReadOnly = true;
+            this.textBox_Result.Size = new System.Drawing.Size(459, 20);
+            this.textBox_Result.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(6, 93);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(459, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 196);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Block Hash";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.Button button_Browse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ BlockHash/BlockHash/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace BlockHash
+{
+    public partial class Form1 : Form
+    {
+
+        const long BLOCK_SIZE = 1024;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Browse_Click(object sender, EventArgs e)
+        {
+
+            FileStream file = null;
+            long file_length = 0;
+            long size_last_block = 0;
+            long nBlocks = 0;
+            long iBlocks = 0;
+            SHA256Managed sha256;
+            byte[] blockHash;
+            byte[] fileChunk;
+            string output;
+
+            sha256 = new SHA256Managed();
+            fileChunk = new byte[BLOCK_SIZE + 256 / 8];
+            blockHash = new byte[256 / 8];
+            output = "";
+            progressBar.Minimum = 1;
+
+            if (openFileDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                try
+                {
+                    progressBar.Value = progressBar.Minimum;
+
+                    file = (FileStream)openFileDialog.OpenFile();
+                    file_length = file.Length;
+
+                    size_last_block = (0 == file_length % BLOCK_SIZE) ? BLOCK_SIZE : file_length % BLOCK_SIZE;
+                    nBlocks = (BLOCK_SIZE == size_last_block) ? file_length / BLOCK_SIZE : file_length / BLOCK_SIZE + 1;
+
+                    file.Position = (nBlocks - 1) * BLOCK_SIZE;
+                    file.Read(fileChunk, 0, (int)size_last_block);
+
+                    blockHash = sha256.ComputeHash(fileChunk, 0, (int)(size_last_block));
+
+                    progressBar.Maximum = (int)(nBlocks - 1);
+                    progressBar.Step = 1;
+                    for (iBlocks = nBlocks - 1; iBlocks > 0; iBlocks--)
+                    {
+                        file.Position = (iBlocks - 1) * BLOCK_SIZE;
+                        file.Read(fileChunk, 0, (int)BLOCK_SIZE);
+
+                        for (int i = 0; i < 256 / 8; i++)
+                        {
+                            fileChunk[BLOCK_SIZE + i] = blockHash[i];
+                        }
+
+                        blockHash = sha256.ComputeHash(fileChunk, 0, (int)(BLOCK_SIZE + 256 / 8));
+
+                        progressBar.PerformStep();
+
+                    }
+
+                    foreach (byte b in blockHash)
+                    {
+                        output += b.ToString("X2");
+                    }
+
+                    textBox_Result.Text = output.ToLower();
+                }
+                catch (Exception)
+                {
+                    
+                    throw;
+                }
+                finally
+                {
+                    if (file != null)
+                    {
+                        file.Close();
+                    }
+                }
+            }
+        }
+    }
+}
